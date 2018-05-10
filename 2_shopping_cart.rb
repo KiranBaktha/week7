c5e1bdeb871e992f58ff16d92aee42ae9cbcb142
Kiran Baktha <sundarambaktha@hotmail.com>
2018-05-09 19:06:01 -0500

    Update 2_shopping_cart.rb

--- 2_shopping_cart.rb
+++ 2_shopping_cart.rb
@@ -1,9 +1,11 @@
-def handle_purchase(cart, credit_card)
+# Does not run. Just an example to show real world version. Code that Jeff worked on for Braintree client.
+
+def handle_purchase(cart, credit_card)  # Took 2 arguments
   logger.info("Starting transaction at #{DateTime.now}")
   success_flag = nil
 
   total = cart.calculate_total
-  if charge_card(credit_card, total)
+  if charge_card(credit_card, total) # Calculate total and charge the card.
     cart.complete = true
     cart.save_to_database
     success_flag = true
@@ -18,9 +20,9 @@ end
 
 def charge_card(card, amount)
   logger.info("Starting to charge the card at #{DateTime.now}")
-  result = PaymentGateway.charge(card.number, amount)
+  result = PaymentGateway.charge(card.number, amount)   # Only PaymentGateway class had to be updated when Paypal bought it.
 
-  if result.status == 'complete'
+  if result.status == 'complete'  # If the status was complete we return transaction code othrwise return false.
     logger.info("Successfully charged card.  Trans Code = #{result.transaction_code}")
     return result.transaction_code
   else
